620ab165db0bcd62bc26f1cadf539b1b64955c9f
James DeLoach <jamesjdeloach@gmail.com>
2024-06-26 17:05:19 -0400

    Changed video scaling

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ function App() {
           <Box>This is the website for the upcoming game <b>Banana Simulator</b> that is currently in development! This website is also still under construction.</Box>
           <Box>Below you will find the first trailer for the game, enjoy! Join our official Discord channel <b><Link href='https://discord.gg/6CMfXWaJTJ' isExternal>here</Link></b> to stay up to date and contribute to the game!</Box>
           <Box paddingTop={5} fontSize={"x-large"}><b>Banana Simulator Trailer 1</b></Box>
-        <iframe src="https://drive.google.com/file/d/1S3Vtft759zC3db0_a0wo9i6Kk6A_kh_9/preview" width="750em" height="420em" allow="autoplay"></iframe>
+        <iframe title="Banana Simulator Trailer 1" src="https://drive.google.com/file/d/1S3Vtft759zC3db0_a0wo9i6Kk6A_kh_9/preview" width="960px" height="540px" allow="autoplay"></iframe>
             <Image w={"12.5%"} src='./spinning_banana.gif' alt='A gif of a spinning wireframe banana.' alignItems={"center"} justifyContent={"center"}></Image>
             <Box paddingTop={5} fontSize={"xx-large"}>Simulating Bananas...<span className="banana-text">10%</span></Box>
             <div className="loading-bar">
